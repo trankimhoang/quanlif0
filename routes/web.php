79c6e20c1cbd9e5d7c532b16724ddd5d8bcaed0e
trankimhoang <trankimhoang11052000@gmail.com>
2022-04-19 19:53:43 +0700

    add page login

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
-    return view('admin.index');
+Route::prefix('/admin')->group(function () {
+   
+
+    Route::get('/index', function() {
+        return view('admin.index');
+    });
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
